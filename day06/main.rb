f682d6627142ed4f87de98cc65d68e7e61ace11b
Lucas Wolschick <6264382+LucasWolschick@users.noreply.github.com>
2025-12-21 12:02:40 -0300

    day 06 code review

--- day06/main.rb
+++ day06/main.rb
@@ -6,30 +6,34 @@ def parse_operator(op_src)
 end
 
 def parse_problem(problem_src)
-  *numbers, op_src = problem_src
-  [numbers.map(&:to_i), parse_operator(op_src)]
+  *numbers_src, op_src = problem_src
+  numbers, op = numbers_src.map(&:to_i), parse_operator(op_src)
+  [numbers, op]
 end
 
 def parse_entries_part01(src)
-  problem_srcs = src.lines.map(&:strip).reject(&:empty?).map(&:split).transpose
-  problem_srcs.map { |problem_src| parse_problem(problem_src) }
+  rows = src.lines.map(&:strip).reject(&:empty?).map(&:split)
+  rows.transpose.map { |problem_src| parse_problem(problem_src) }
 end
 
 def parse_entries_part02(src)
-  transposed_src = src.lines.map(&:chars).transpose.map(&:join).map(&:strip).join("\n")
-  transposed_src.split("\n\n").map do |problem_src|
+  cols = src.lines.map(&:chars).transpose
+  transposed_src = cols.map(&:join).map(&:strip).join("\n")
+  problem_srcs = transposed_src.split("\n\n")
+
+  problem_srcs.map do |problem_src|
     lines = problem_src.lines.map(&:strip)
-    op = lines.first.chars.last
+    op = lines.first[-1]
     numbers = lines.map { |line| line.tr(" *+", "") }
     parse_problem(numbers + [op])
   end
 end
 
 def solve(data)
-  data.sum do |problem|
-    case problem[1]
-    when :add then problem[0].sum
-    when :mul then problem[0].reduce(1, &:*)
+  data.sum do |numbers, op|
+    case op
+    when :add then numbers.sum
+    when :mul then numbers.reduce(1, &:*)
     end
   end
 end
